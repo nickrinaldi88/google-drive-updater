ac92e52ebd30593e546f2192497b493490181a62
Nick Rinaldi <nickrinaldi@macbook-pro.mynetworksettings.com>
2023-04-20 00:51:57 -0400

    add service account import; added function code block outside of function to test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 import os
 import json
-from google.oauth2.credentials import Credentials
+from google.oauth2.service_account import Credentials
 from googleapiclient.errors import HttpError
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
@@ -14,7 +14,7 @@ with open('secrets.json', 'r') as f:
 time_interval = 3600  # upload a file every hour
 
 # Set the path to the folder on your desktop
-desktop_folder = '/Users/nickrinaldi/Desktop'
+desktop_folder = '/Users/nickrinaldi/Desktop/Mixing-Music/Desktop'
 
 # Set the ID of the folder in your Google Drive account to upload files to
 folder_id = secrets['folder_id']
@@ -24,13 +24,32 @@ mime_type = 'audio/mpeg'
 
 # Set the Google Drive API version and credentials
 api_version = 'v3'
-creds = Credentials.from_authorized_user_file('credentials.json')
+creds = Credentials.from_service_account_file('credentials.json')
+
+
+service = build('drive', api_version, credentials=creds)
+
+    file_metadata = {'parents': [folder_id]}
+    media = MediaFileUpload(file_path, mimetype=mime_type)
+
+    try:
+        file = service.files().create(
+            body=file_metadata,
+            media_body=media,
+            fields='id'
+        ).execute()
+        print(f'File ID: {file.get("id")} uploaded successfully.')
+    except HttpError as error:
+        print(f'An error occurred: {error}')
+        file = None
 
 
 def upload_file_to_drive(file_path, folder_id, mime_type):
     """
     Uploads a file to a folder in Google Drive using the Google Drive API.
     """
+
+    # builds a file 
     service = build('drive', api_version, credentials=creds)
 
     file_metadata = {'parents': [folder_id]}
